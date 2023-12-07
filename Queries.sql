857e859e9e1af71a08981f4f7396ee5d10ee2578
Mari <aura.haze.development@gmail.com>
2023-12-07 18:20:18 -0500

    queries done

---
+++ Queries.sql
@@ -0,0 +1,10 @@
+﻿SELECT Customers.CustomerName
+FROM Orders
+INNER JOIN Customers ON Orders.CustomerID=Customers.CustomerID
+Where OrderID=10310
+
+
+SELECT Suppliers.Address
+FROM Products
+INNER JOIN Suppliers ON Suppliers.SupplierID=Products.SupplierID
+Where ProductID=40
